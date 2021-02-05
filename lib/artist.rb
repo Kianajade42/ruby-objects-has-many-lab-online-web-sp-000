e3ec00a8b40f20dec4f1678c77c964a62f81c5d3
Kiana Long <kianajade42.kl@gmail.com>
2021-02-05 22:11:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,13 @@
 class Artist
   attr_accessor :name
-
+  @@all=[]
   def initialize(name)
     @name=name
+    @@all << self
   end
-
-
-
+def songs
+  Song.all.select {|song|| song.artist ==self}
+end
 
 
 end
